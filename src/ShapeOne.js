56f74799fbb2918f51d5446727044c2e12b16c17
Daniel Williams <danielpaulwill@gmail.com>
2023-02-11 20:50:04 -0500

    first shape clicking working

--- src/ShapeOne.js
+++ src/ShapeOne.js
@@ -1,8 +1,25 @@
+import { useEffect, useState } from 'react';
 
 function ShapeOne() {
+  const [shapeCount, setShapeCount] = useState(1)
+  const [shape, setShape] = useState()
+
+  useEffect(() => {
+    if (shapeCount === 1) {
+      setShape("square")
+    } else if (shapeCount === 2) {
+      setShape("circle")
+    } else if (shapeCount === 3) {
+      setShape("triangle")
+    } else setShapeCount(1)
+  }, [shapeCount])
+
+  function handleShapeClick() {
+    setShapeCount(shapeCount => ++shapeCount)
+  }
+
   return (
-    <div className="App">
-      <h1>ShapeOne</h1>
+    <div className={shape} onClick={handleShapeClick}>
     </div>
   );
 }
